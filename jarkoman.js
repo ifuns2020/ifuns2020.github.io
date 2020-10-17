8614824ce4d1473cdcbac147212b74db85e21734
Michael R. Krisnadhi <michaelkrisnadhi@gmail.com>
2020-10-17 19:03:25 +0700

    update page & info algoritmaru

--- jarkoman.js
+++ jarkoman.js
@@ -8,18 +8,30 @@ var informations = {
             title: 'Algoritmaru',
             items: [
                 [
-                    '### List jawaban',
-                    '1. **Gedung B Lt. 4**',
-                    '2. **quick_sort**',
-                    '3. **3192**'
+                    '### Info',
+                    '- Website: <https://himaster.mipa.uns.ac.id/algoritmaru>',
+                    '- Target 15 soal terjawab per Sabtu, 24 Oktober 2020',
+                    '- List kontak kakak asuh (bisa di PC tuh): [KLIK DI SINI](https://drive.google.com/file/d/1mZh0lZyKtDrvKgv3vg_vTn15OA-6T81K/view?usp=sharing)',
                 ].join('\n'),
-                'Kalo udah isi ceklis di [sini](https://bit.ly/DhinAlgoritmaru)',
-            ]
-        },
-        {
-            title: 'Karya Tulis Ilmiah',
-            items: [
-                'Udah beres yeayy'
+                [
+                    '### List Jawaban',
+                    '_NB penulisan harus sama persis ya, huruf besar kecil dan tanda bacanya juga_',
+                    '1. `Gedung B Lt. 4`',
+                    '2. `quick_sort`',
+                    '3. `3192`',
+                    '4. `Mr. Ardhi Wijayanto`',
+                    '5. `https://github.com/adityaazizi`',
+                    '6. `ILOVEU`',
+                    '7. `Switch Allied Telesis AT-FS750`',
+                    '8. `Update Table Routing`',
+                    '9. `???` (seputar Gennady Korotkevich, clue: mengalihkan, petualang, 1 atau 2 kata, competitive programming, messi, ural, 2 benua, pelajari bahasa soviet, contest, poland lithuania latvia ukraina rusia)',
+                    '',
+                    '**Kalo udah jawab sampai no 8 isi ceklis di [sini](https://docs.google.com/spreadsheets/d/1cDxlTGAK_Nu1Yd16yON_M_kBwO9KZl2RPDHEV217r0s/edit?usp=sharing) ya (buka di PC aja)**',
+                ].join('\n'),
+                [
+                    '### Rules Chat Kating',
+                    '1. Nanti diinfokan lagi ya :D'
+                ].join('\n')
             ]
         }
     ]
